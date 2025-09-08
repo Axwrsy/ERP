536ba2fd6beda11f7819313ecbb7316d6346eeef
carol <annasantos4530@gmail.com>
2025-09-08 16:00:47 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import Grafico from './components/Grafico'
 import Header from './components/header';
 import './components/css/Header.css'
+import './components/css/Grafico.css'
 
 
 
@@ -11,6 +12,7 @@ function App(){
   return(
     <div>
     <Header/>
+    <Grafico/>
     </div>
   )
 }
